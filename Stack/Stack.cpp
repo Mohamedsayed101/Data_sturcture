a3b68767b0ec67c5ec6536542f33fbecbe304b5f
Mohamedsayed101 <mo.sayedomar123@gmail.com>
2025-01-31 17:10:57 +0200

    create function push and display and part of main

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -3,7 +3,69 @@
 
 using namespace std;
 
+class Node {
+    public:
+    
+    int data;
+    Node* next;
+    
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+class LinkedStack {
+    public:
+    Node* top;
+    LinkedStack(){
+        top = NULL;
+    }
+    bool IsEmpty(){
+        return (top == nullptr);
+    }
+    void push(int NewItem)
+    {
+        Node *n2 = new Node(NewItem);
+        n2->next = nullptr;
+
+        if (IsEmpty())
+        {
+            top = n2;
+        }
+        else
+        {
+            n2->next = top;
+            top = n2;
+        }
+    }
+    int pop(){
+        
+    }
+    void display()
+    {
+        Node* temp = top;
+        cout<<"Stack: \n";
+        int i=1;
+        while(temp!=NULL)
+        {
+
+            cout<<"Item "<<i++<<" = "<<temp->data<<endl;
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+};
 int main(){
+        LinkedStack Stack;
+    for (size_t i = 0; i < 5; i++)
+    {
+        cout<<"Enter Item "<<i+1<<": ";
+        int item;
+        cin>>item;
+        Stack.push(item);
+    }
+    Stack.display();
+    
 
 
     return 0;
